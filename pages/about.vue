7a052af17453a1ce8d28bd3ca6770ad2a71815b8
Nir Yasoor <38460533+niryas@users.noreply.github.com>
2025-04-09 18:01:43 +0300

    feat: about page

---
+++ pages/about.vue
@@ -0,0 +1,194 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div>
+		<!--    Hero section start    -->
+		<section
+			class="innpage-hero-section"
+			style="background-color: #62CBFB;">
+			<img
+				src="/images/cloudwhite.webp"
+				class="hero-left-shape"
+				alt="">
+			<div class="container">
+				<div class="row align-items-center gx-0 mt-40">
+					<div class="col-xl-12">
+						<div class="hero-content text-center">
+							<h1>About Joyful Orlando</h1>
+						</div>
+						<img
+							src="/images/white-star.webp"
+							class="hero-star-shape"
+							alt="">
+					</div>
+				</div>
+			</div>
+			<img
+				src="/images/colorscloud.webp"
+				class="hero-right-shape"
+				alt="">
+		</section>
+		<!--    Hero section end    -->
+
+		<!--    Event list section start    -->
+		<section class="sec-padding">
+			<div class="container">
+				<div class="sec-heading sec-head-has-shape sec-head-center mb-0">
+					<div class="row justify-content-center">
+						<div class="col-xl-8  position-relative">
+							<h2 class="mb-3">
+								Welcome to Joyful Orlando!
+							</h2>
+							<p>Joyful Orlando is on a mission to spotlight conscious & wellness events that help you slow down, show up, and feel actually good (not just “pretend good” for Instagram). Joyful Orlando is on a mission to spotlight conscious & </p>
+							<div class="shead-shape">
+								<img
+									src="/images/yellow-star.svg"
+									alt="">
+								<img
+									src="/images/purple-star.svg"
+									class="purple-star"
+									alt="">
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-12">
+							<div class="video-content position-relative">
+								<img
+									src="/images/video.webp"
+									class="img-fluid"
+									alt="">
+								<a
+									class="video-btn"
+									role="button"
+									data-bs-toggle="modal"
+									data-src="https://www.youtube.com/watch?v=DUiYVJIVNcA"
+									data-bs-target="#myModal">
+									<i class="fa fa-play" />
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--    Event list section end    -->
+
+		<!--   Mission section start   -->
+		<section
+			class="sec-padding position-relative"
+			style="background-image: url(/images/missionbg.webp);">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 ">
+						<div class="mission-left">
+							<img
+								src="/images/mission.png"
+								class="img-fluid"
+								alt="">
+						</div>
+					</div>
+					<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 align-content-center">
+						<div class="mission-content-right ">
+							<div class="title-content">
+								<h2>Our Mission</h2>
+							</div>
+							<p>
+								Joyful Orlando is on a mission to spotlight conscious & wellness events that help you slow down, show up, and feel actually good (not just “pretend good” for Instagram). Joyful Orlando is on a mission to spotlight conscious & <br> <br>
+
+								Lorem ipsum dolor sit amet consectetur. Proin nibh duis quis mattis ut habitasse.. Augue amet in mauris semper. Sed sit neque cursus non dictum libero vel commodo id.. Faucibus tincidunt pharetra nibh nisl euismod pellentesque.. Neque aliquet nec pellentesque dignissim.
+							</p>
+							<div class="mission-steps">
+								<div>
+									<div class="outer-box">
+										<i
+											class="fa fa-check-circle"
+											style="color: #FE7D15;" />
+										<p>Psychodynamic Therapy</p>
+									</div>
+									<div class="outer-box">
+										<i
+											class="fa fa-check-circle"
+											style="color: #9F64FC;" />
+										<p>Consultation</p>
+									</div>
+								</div>
+								<div>
+									<div class="outer-box">
+										<i
+											class="fa fa-check-circle"
+											style="color: #62CBFB;" />
+										<p>Couple Problem</p>
+									</div>
+									<div class="outer-box">
+										<i
+											class="fa fa-check-circle"
+											style="color: #FF839F;" />
+										<p>Mental Health</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="right-vector">
+				<img
+					src="/images/right-vector.webp"
+					alt="">
+			</div>
+		</section>
+		<!--   Mission section end   -->
+
+		<!--   team section start  -->
+
+		<section class="sec-padding">
+			<div class="container">
+				<div class="sec-heading  sec-head-center">
+					<div class="row">
+						<div class="col">
+							<h2>Our Team</h2>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 team-outer">
+							<div class="team-member-detail">
+								<div class="member-profile">
+									<img
+										src="/images/member1.webp"
+										alt="">
+								</div>
+								<div class="team-detail">
+									<p>Joyful Orlando is on a mission to spotlight conscious & wellness events that help you slow down, show up, and feel actually good (not just “pretend good” for Instagram). Joyful Orlando is on a mission to spotlight conscious </p>
+									<h6>Johnthan Doe</h6>
+									<span>CEO</span>
+								</div>
+							</div>
+						</div>
+						<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 team-outer">
+							<div class="team-member-detail">
+								<div class="member-profile">
+									<img
+										src="/images/member2.webp"
+										alt="">
+								</div>
+								<div class="team-detail">
+									<p>Joyful Orlando is on a mission to spotlight conscious & wellness events that help you slow down, show up, and feel actually good (not just “pretend good” for Instagram). Joyful Orlando is on a mission to spotlight conscious </p>
+									<h6>Catherine Lubowitz</h6>
+									<span>CEO</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	<!--   team section end  -->
+	</div>
+</template>
+
+<style scoped>
+
+</style>
