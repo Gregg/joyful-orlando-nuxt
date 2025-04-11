8c68f7a4141cf1f13910bc6afde8b9284be5bb0e
Nir Yasoor <38460533+niryas@users.noreply.github.com>
2025-04-11 17:10:53 +0300

    refactor: create temp-data/tempEvents to populate events before connecting to backend

--- pages/events.vue
+++ pages/events.vue
@@ -1,24 +1,5 @@
 <script setup lang="ts">
-const events = [
-	{
-		id: "0",
-		name: "New Moon Community Event and Drum Circle",
-		date: "February 2, 4 PM",
-		imageUrl: "/images/event-1.webp",
-	},
-	{
-		id: "1",
-		name: "Cuddles & Consent: Platonic Touch in a Facilitated Cuddle Space",
-		date: "February 23, 6:30 PM",
-		imageUrl: "/images/event-2.webp",
-	},
-	{
-		id: "2",
-		name: "Akashic Horizons: Group Meditation Experience",
-		date: "February 12, 1 PM",
-		imageUrl: "/images/event-3.webp",
-	},
-];
+import { events } from "~/data-temp/tempEvents";
 </script>
 
 <template>
