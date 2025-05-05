000451f306aa901bedcd982b64324df233326865
Nir Yasoor <38460533+niryas@users.noreply.github.com>
2025-05-05 13:21:20 +0300

    fix: load bootstrap locally to fix css-order bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,6 @@
 export default defineNuxtConfig({
 	modules: ["@nuxt/eslint", "@vueuse/nuxt", "@nuxtjs/seo"],
 	devtools: { enabled: true },
-	app: {
-		head: {
-			link: [
-				{
-					rel: "stylesheet",
-					href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css",
-					integrity: "sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7",
-					crossorigin: "anonymous",
-				},
-			],
-		},
-	},
 	css: ["~/assets/styles/style.css", "~/assets/styles/responsive.css", "~/assets/styles/all.css"],
 	site: {
 		// url: "", // Site URL Here
