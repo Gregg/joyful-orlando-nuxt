e38f7423030dd195f08ea19c2a71f916cc19b216
Nir Yasoor <38460533+niryas@users.noreply.github.com>
2025-05-14 14:51:47 +0300

    fix: imageUrl comes as an array from airtable

--- server/api/events.ts
+++ server/api/events.ts
@@ -6,13 +6,16 @@ const events: Array<IEvent> = [];
 async function fetchEvents() {
 	await base("Events").select().eachPage((records, processNextPage) => {
 		records.forEach(function (record) {
+			const imageArray = record.get("ImageUrl") as Array<string>;
+			const imageUrl = imageArray[0] ? imageArray[0] : "";
+
 			const fetchedEvent: IEvent = {
 				id: record.id,
 				name: record.get("Name") as string,
 				date: record.get("Date") as string,
 				locationId: (record.get("Location") as Array<string>)[0] as string,
 				description: record.get("Description") as string,
-				imageUrl: record.get("ImageUrl") as string,
+				imageUrl,
 				slug: record.get("Slug") as string,
 				categories: record.get("Categories") as Array<string>,
 				featured: record.get("Featured") as boolean,
