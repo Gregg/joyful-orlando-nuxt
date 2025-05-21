134e41ae61735a4cb884b43517de810e65361552
Nir Yasoor <38460533+niryas@users.noreply.github.com>
2025-05-21 16:58:43 +0300

    feat: /giveaway page

---
+++ pages/giveaway.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+onMounted(() => {
+	const script = document.createElement("script");
+	script.src = "https://sweepwidget.com/w/j/w_init.js";
+	script.async = true;
+	document.body.appendChild(script);
+});
+</script>
+
+<template>
+	<div>
+		<!--    Hero section start    -->
+		<section
+			class="innpage-hero-section"
+			style="background-color: #62CBFB;">
+			<img
+				src="/images/cloudwhite.webp"
+				class="hero-left-shape"
+				alt="">
+			<div class="container">
+				<div class="row align-items-center gx-0 mt-40">
+					<div class="col-xl-12">
+						<div class="hero-content text-center">
+							<h1>Giveaway</h1>
+						</div>
+						<img
+							src="/images/white-star.webp"
+							class="hero-star-shape"
+							alt="">
+					</div>
+				</div>
+			</div>
+			<img
+				src="/images/colorscloud.webp"
+				class="hero-right-shape"
+				alt="">
+		</section>
+		<!-- Hero section ends -->
+
+		<!--    Giveaway section start    -->
+		<section>
+			<div
+				id="90656-ibxpestf"
+				class="sw_container" />
+		</section>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
