05b70d0d1c958a97342bff0bd0993ad91dd2db77
ivadhera <ishaanvadhera@gmail.com>
2025-06-12 09:35:02 -0400

    Update useGiveaway.ts
    Day 12. Keeping 11 as place holder for now.

--- composables/useGiveaway.ts
+++ composables/useGiveaway.ts
@@ -63,8 +63,8 @@ const giveawaySchedule: GiveawayDay[] = [
   },
   // Day 12 (June 12th 12:00 AM - June 13th 12:00 AM)
   {
-    bannerText: "Win a _____ at _______ - Day 12 of 14 Days of Joy!",
-    giveawayId: "replace_with_id"
+    bannerText: "Win a FREE Soulkit by Rise Up Live Bright - Day 12 of 14 Days of Joy!",
+    giveawayId: "91197-lijoxpbk"
   },
   // Day 13 (June 13th 12:00 AM - June 14th 12:00 AM)
   {
